cef6e5af6a6c6a35d156030e692e0636757129d6
Damian <dudussk@go2.pl>
2026-02-17 15:20:20 +0100

    Solution5

--- app/main.py
+++ app/main.py
@@ -12,15 +12,18 @@ class Person:
 def create_person_list(people: list) -> list:
     # write your code here
     pass
-    [Person(p.get("name"), p.get("age")) for p in people]
+    Person.people = {}
+    [Person(person.get("name"), person.get("age")) for person in people]
     result_list = []
     for person in people:
         person_instance = Person.people.get(person("name"))
         wife_name = person.get("wife")
         husband_name = person.get("husband")
-        if wife_name:
-            setattr(person_instance, "wife", wife_name)
-        if husband_name:
-            setattr(person_instance, "husband", husband_name)
+        if wife_name is not None:
+            setattr(person_instance, "wife",
+                    Person.people.get(wife_name))
+        if husband_name is not None:
+            setattr(person_instance, "husband",
+                    Person.people.get(husband_name))
         result_list.append(person_instance)
     return result_list
