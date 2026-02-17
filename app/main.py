0053a9784bac1559f2a536dc7af62fb0d374b06e
Damian <dudussk@go2.pl>
2026-02-17 14:18:49 +0100

    Solution2

--- app/main.py
+++ app/main.py
@@ -9,16 +9,14 @@ class Person:
 
 def create_person_list(people: list) -> list:
     for person in people:
-        Person(person.name, person.age)
+        Person(person["name"], person["age"])
     result_list = []
     for person in Person.people:
         person_instance = Person.people[person["name"]]
         spouse_key = "wife" if "wife" in person else "husband"
         spouse_name = person.get(spouse_key)
-        if spouse_name:
+        if spouse_name is not None:
             spouse_instance = Person.people.get(spouse_name)
             setattr(person_instance, spouse_key, spouse_instance)
-        else:
-            setattr(person_instance, spouse_key, None)
         result_list.append(person_instance)
     return result_list
